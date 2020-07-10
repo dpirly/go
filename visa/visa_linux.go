95cc74dcdc7ad5b172ec53299a1e634ef324e9e7
dpirly <dpirly@126.com>
2020-07-10 19:51:34 +0800

    [visa] write helper function to call C function

--- visa/visa_linux.go
+++ visa/visa_linux.go
@@ -3,7 +3,24 @@ package visa
 /*
 #cgo linux CFLAGS: -I/usr/include/rsvisa
 #cgo linux LDFLAGS: -lrsvisa
+#include <stdlib.h> //for free()
+#include <stdint.h>
 #include <visa.h>
+
+inline static uint32_t write(ViSession s, _GoString_ cmd){
+	ViUInt32 ret;
+	viWrite(s, (ViBuf)_GoStringPtr(cmd), _GoStringLen(cmd), &ret);
+	return (uint32_t)ret;
+}
+
+inline static uint32_t query(ViSession s, _GoString_ cmd, uint8_t* resp, int len){
+	ViUInt32 ret = write(s, cmd);
+	if (ret == 0){
+		return len;
+	}
+	viRead(s, resp, len, &ret);
+	return (uint32_t)ret;
+}
  */
 import "C"
 import (
@@ -21,9 +38,15 @@ func init(){
 }
 
 func Open(res string, timeout int32) (s C.ViSession, err error){
-	r := C.viOpen(rm, (*C.ViChar)(unsafe.Pointer(C.CString(res))), 4, C.ViUInt32(timeout), &s)
+	// convert to C string, which is ends with '\0'
+	name := C.CString(res)
+	r := C.viOpen(rm, (*C.ViChar)(unsafe.Pointer(name)), C.VI_LOAD_CONFIG, C.ViUInt32(timeout), &s)
+
+	// need free the string
+	C.free(unsafe.Pointer(name))
+
 	if r != C.VI_SUCCESS{
-		return s, errors.New(fmt.Sprintf("error is %d", r))
+		return s, errors.New(fmt.Sprintf("viOpen error, code is %d", r))
 	}
 	return s, err
 }
@@ -32,16 +55,13 @@ func (s C.ViSession) Close(){
 	C.viClose(C.ViObject(s))
 }
 
-func (s C.ViSession) Write(cmd string) int32{
-	var retCnt int32
-	C.viWrite(s, (*C.ViByte)(unsafe.Pointer(C.CString(cmd))), C.ViUInt32(len(cmd)), (*C.ViUInt32)(unsafe.Pointer(&retCnt)))
-	return retCnt
+func (s C.ViSession) Write(cmd string) uint32{
+	return uint32(C.write(s, cmd))
 }
 
 func (s C.ViSession) Query(cmd string) string{
-	s.Write(cmd)
-	var retCnt int32
-	var buf = make([]byte, 1024)
-	C.viRead(s, (*C.ViByte)(unsafe.Pointer(&buf[0])), 1024, (*C.ViUInt32)(unsafe.Pointer(&retCnt)))
-	return string(buf)
+	resp := make([]byte, 128)
+
+	C.query(s, cmd, (*C.uint8_t)(&resp[0]), C.int(cap(resp)))
+	return string(resp)
 }
